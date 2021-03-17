1793fe3da2810ebaa9c11aafc83ad6bbbdfb2fc4
jemin park <parkjm51495@gmail.com>
2021-03-17 17:50:34 +0900

    database

---
+++ config/config.js
@@ -0,0 +1,24 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const config = {
+  development: {
+    host: process.env.DATABASE_SPRINT_HOST,
+    username: process.env.DATABASE_SPRINT_USER,
+    password: process.env.DATABASE_SPRINT_PASSWORD,
+    database: 'dailyemotion',
+    dialect: 'mysql',
+    logging: false
+
+  },
+  test: {
+    host: process.env.DATABASE_SPRINT_HOST,
+    username: process.env.DATABASE_SPRINT_USER,
+    password: process.env.DATABASE_SPRINT_PASSWORD,
+    database: 'dailyemotion',
+    dialect: 'mysql',
+    logging: false
+  }
+};
+
+module.exports = config;
