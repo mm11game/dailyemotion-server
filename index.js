29144ea580d49acce2044f078ab7d1fc8311eaef
jemin park <parkjm51495@gmail.com>
2021-03-24 10:36:58 +0900

    T

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express = require('express');
-const https = require('https')
-const fs = require('fs')
-const cors = require('cors')
-const session = require('express-session')
-require('./models')
+const https = require('https');
+const fs = require('fs');
+const cors = require('cors');
+const session = require('express-session');
+require('./models');
 const app = express();
-const textController = require('./controllers/text')
-const userController = require('./controllers/user')
+const textController = require('./controllers/text');
+const userController = require('./controllers/user');
 const PORT = 5000;
-app.use(cors({
-    origin: "*",
-    credentials:true}))
+app.use(cors({credentials:true}));
 app.use(express.json()); 
 app.use(express.urlencoded( {extended : false } ));
 app.use(
@@ -26,7 +24,7 @@ app.use(
             secure: true
         }
     })
-)
+);
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', req.headers.origin);
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -36,23 +34,23 @@ app.use(function(req, res, next) {
 // app.use('/', (req,res)=>{
 //     res.send('hello every one')
 // })// hello every one위해서 넣음
-app.get("/user/userInfo",userController.userInfo)
-app.get("/text/textList",textController.textList)
-app.get("/text/garbageList",textController.garbageList)
+app.get("/user/userInfo",userController.userInfo);
+app.get("/text/textList",textController.textList);
+app.get("/text/garbageList",textController.garbageList);
 
-app.post("/user/login",userController.login)
-app.post("/user/signup",userController.signup)
-app.post("/user/signout",userController.signout)
-app.post("/user/change",userController.change)
-app.post("/text/textRecord",textController.textRecord)
-app.post("/text/textChange",textController.textChange)
-app.post("/text/finalDelete",textController.finalDelete)
-app.post("/text/goToGarbage",textController.goToGarbage)
-app.post("/text/undo",textController.undo)
-app.post("/text/test",textController.test1)
-app.get("/user",userController.user)
+app.post("/user/login",userController.login);
+app.post("/user/signup",userController.signup);
+app.post("/user/signout",userController.signout);
+app.post("/user/change",userController.change);
+app.post("/text/textRecord",textController.textRecord);
+app.post("/text/textChange",textController.textChange);
+app.post("/text/finalDelete",textController.finalDelete);
+app.post("/text/goToGarbage",textController.goToGarbage);
+app.post("/text/undo",textController.undo);
+app.post("/text/test",textController.test1);
+app.get("/user",userController.user);
 
-// let server
+// let server;
 // if(fs.existsSync("./key.pem") && fs.existsSync("./cert.pem")){
 //     server = https
 //       .createServer(
@@ -74,6 +72,6 @@ let server = https.createServer(
       cert: fs.readFileSync('/etc/letsencrypt/live/projectb1.com/cert.pem')
             
 },app)
-.listen(PORT)
+.listen(PORT);
 
 module.exports = server;
